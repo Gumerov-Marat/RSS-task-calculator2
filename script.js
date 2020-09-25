bfb0a157098295b858f5bd3b0d8135719b855e39
Marat Gumerov <gumerovm74@gmail.com>
2020-09-25 15:11:50 +0300

    ready base

--- script.js
+++ script.js
@@ -96,8 +96,9 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const previousOperandTextElement = document.querySelector('[data-previos-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
