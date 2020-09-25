fbd003b6589645bc6a3988802a285bab9959d83e
Marat Gumerov <gumerovm74@gmail.com>
2020-09-25 22:02:11 +0300

    refactoring

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previos-operand]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
 
